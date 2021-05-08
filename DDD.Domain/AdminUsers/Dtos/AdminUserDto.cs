98f24a6fec69552e3ead0a0b242e88a4bb5e9edf
Nigel <Nigel@PS-03>
2021-05-08 13:53:39 +0800

    缩减代码

--- DDD.Domain/AdminUsers/Dtos/AdminUserDto.cs
+++ DDD.Domain/AdminUsers/Dtos/AdminUserDto.cs
@@ -13,16 +13,7 @@ namespace DDD.Domain.AdminUsers
         public string Position { get; set; }
         public string Company { get; set; }
 
-        public override void Mapping(Profile profile)
-        {
+        public override void Mapping(Profile profile) =>
             profile.CreateMap<AdminUserEntity, AdminUserDto>();
-            //.ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
-            //.ForMember(d => d.Mobile, opt => opt.MapFrom(s => s.Mobile))
-            //.ForMember(d => d.Name, opt => opt.MapFrom(s => s.Name))
-            //.ForMember(d => d.Picture, opt => opt.MapFrom(s => s.Picture))
-            //.ForMember(d => d.Location, opt => opt.MapFrom(s => s.Location))
-            //.ForMember(d => d.Position, opt => opt.MapFrom(s => s.Position))
-            //.ForMember(d => d.Company, opt => opt.MapFrom(s => s.Company));
-        }
     }
 }
