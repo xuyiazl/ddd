d39bbe3f12a1894ad973e431b8fba3a8caaf94b7
Nigel <3624091@qq.com>
2021-06-11 16:24:24 +0800

    修复swagger不分组

--- DDD.Applaction/SwaggerDependencyInjection.cs
+++ DDD.Applaction/SwaggerDependencyInjection.cs
@@ -57,7 +57,7 @@ namespace DDD.Applaction
                 options.SwaggerControllerDescriptions(apiXml);
 
                 // TODO:一定要返回true！
-                options.DocInclusionPredicate((docName, description) => true);
+                //options.DocInclusionPredicate((docName, description) => true);
             });
 
             return services;
